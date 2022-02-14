dcf7125aaf873ad0593d3cf9bb63a0b18940ff9b
Ali baba <seningibimailinben@gmail.com>
2022-02-14 14:28:22 +0100

    first react assignment

---
+++ Node/calculate.js
@@ -0,0 +1,10 @@
+const addition = (n1, n2) => {
+    return n1 + n2 ;
+};
+
+
+function multiplication(n1, n2) {
+    return n1 * n2
+};
+
+module.exports = {addition, multiplication};// export edilir
---
+++ Node/index.js
@@ -0,0 +1,5 @@
+const calc = require("./calculate"); //inport yapilir
+
+// console.log(calc.addition(2, 5));
+// console.log(`selam  ${calc.multiplication(2, 5)}`)
+
