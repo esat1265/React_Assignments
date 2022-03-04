92b95a8fe55ddf33d84cc3ab05cb613c9f65a7c4
Ali baba <seningibimailinben@gmail.com>
2022-03-04 15:20:45 +0100

    Assignment has been completed

--- Week_03/quiz-application/src/App.js
+++ Week_03/quiz-application/src/App.js
@@ -1,58 +1,139 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import cardList from './components/cardList.js'
+import swal from 'sweetalert';
 
 function App() {
 
   const questions = [
     {
       id : 1 ,
-      answer : "banana",
       options : [
-        "banana", "lemon","kiwi","apple"]
+        {fruit:"banana", answer: true},
+        {fruit:"lemon", answer: false},
+        {fruit:"kiwi", answer: false},
+        {fruit:"apple", answer: false}
+        ]
     },
     {
       id : 2 ,
-      answer : "lime",
       options : [
-        "lime", "orange","melon","watermelon"]
+        {fruit:"lime", answer: false},
+        {fruit:"orange", answer: false},
+        {fruit:"melon", answer: true},
+        {fruit:"watermelon", answer: false}
+        ]
     },
     {
       id : 3 ,
-      answer : "grape",
       options : [
-        "strawberry", "mango","peach","grape"]
-    }, 
+        {fruit:"strawberry", answer: false},
+        {fruit:"mango", answer: false},
+        {fruit:"grape", answer: false},
+        {fruit:"peach", answer: true}
+        ]
+    },
     {
       id : 4 ,
-      answer : "fig",
       options : [
-        "pineapple", "fig","blachberry","apple"]
-    }, 
+        {fruit:"pineapple",answer: false},
+        {fruit:"fig", answer: true},
+        {fruit:"blachberry", answer: false},
+        {fruit:"apple", answer: false}
+        ]
+    },
     {
       id : 5 ,
-      answer : "poire",
       options : [
-        "date", "poire","kiwi","pineapple"]
+        {fruit:"date",answer: true},
+        {fruit:"poire", answer: false},
+        {fruit:"kiwi", answer: false},
+        {fruit:"pineapple", answer: false}
+        ]
+    },{
+      id : 6 ,
+      options : [
+        {fruit:"banana", answer: true},
+        {fruit:"lemon", answer: false},
+        {fruit:"kiwi", answer: false},
+        {fruit:"apple", answer: false}
+        ]
     },
+    {
+      id : 7 ,
+      options : [
+        {fruit:"lime", answer: false},
+        {fruit:"orange", answer: false},
+        {fruit:"melon", answer: true},
+        {fruit:"watermelon", answer: false}
+        ]
+    },
+    {
+      id : 8 ,
+      options : [
+        {fruit:"strawberry", answer: false},
+        {fruit:"mango", answer: false},
+        {fruit:"grape", answer: false},
+        {fruit:"peach", answer: true}
+        ]
+    },
+    {
+      id : 9 ,
+      options : [
+        {fruit:"pineapple",answer: false},
+        {fruit:"fig", answer: true},
+        {fruit:"blachberry", answer: false},
+        {fruit:"apple", answer: false}
+        ]
+    },
+    {
+      id : 10 ,
+      options : [
+        {fruit:"date",answer: true},
+        {fruit:"poire", answer: false},
+        {fruit:"kiwi", answer: false},
+        {fruit:"pineapple", answer: false}
+        ]
+    }
   ];
 
-  const handleAnswer = (answer)=>{
-    if(answer === questions[0].answer){
-      alert('vavv')
-    }
+
+ const [currentQuestion, setCurrentQuestion] = useState(0);
+ const [correctAnswer, setCorrectAnswer] = useState(0);
+ const [falseAnswer, setFalseAnswer] = useState(0);
+
+ 
+ const nextQuestion = () => {
+  const nextQuestion = currentQuestion + 1;
+  if(nextQuestion < questions.length ){
+    setCurrentQuestion(nextQuestion)
   }
+ };
+
+ const handleAnswer = (answer)=>{
+  if(answer === true){
+    swal("Good job!", "Tebrikler BILDINIZ!", "success");
+    const correctScore = correctAnswer +1;
+    setCorrectAnswer(correctScore)
+  }else {
+    swal("Sorry!", "Uzgunum BILEMEDINIZ! Diger soruya geciniz", "error");
+    const falseScore = falseAnswer +1;
+    setFalseAnswer(falseScore)
+  }
+};
+
+
+
   return (
   
     <div className="App">
       <div className='header'>WORD CARD QUIZ</div>
-      <button className="next">NEXT</button>
-      <div className='number-of-question'>QUESTION 1</div>
+      <button onClick={nextQuestion}className="next">NEXT</button>
+      <div className='number-of-question'>QUESTION {currentQuestion +1}</div>
       <div className='option-part'>
-         {questions[0].options.map(question => <button onClick={handleAnswer({question})}className='options'>{question}</button> )}
+         {questions[currentQuestion].options.map(question => <button key={question.id} onClick={()=>handleAnswer(question.answer)}className='options'>{question.fruit}</button> )}
       </div>
-      <div className='true-score'>True Score 1</div>
-      <div className='false-score'>False Score 1</div>
+      <div className='true-score'>True Score {correctAnswer}</div>
+      <div className='false-score'>False Score {falseAnswer}</div>
     </div>
   
   );
