d25106f682848331c2b80a527d42866f835c54e3
Ali baba <seningibimailinben@gmail.com>
2022-03-12 12:22:26 +0100

    assaignment has been completed

--- Week_04/api/src/component/Api.js
+++ Week_04/api/src/component/Api.js
@@ -1,26 +1,49 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import React from "react";
 
 function Api() {
+  const [api, setApi] = useState();
 
-   const [api, setApi] = useState(); 
+  useEffect(() => {
+    getApi();
+  }, []);
 
-   const getApi = async () => {
+  const getApi = async () => {
     try {
-        const response = await fetch('https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely');
-        const data = await response.json();
-        setApi(data)
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await response.json();
+      setApi(data);
     } catch (error) {
-        console.log("Error")
+      console.log("Error");
     }
-   };
-
-   useEffect(()=>{getApi()},[]);
+  };
 
   return (
-    
-    console.log(api)
-    
-  )
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">NAME</th>
+            <th scope="col">USERNAME</th>
+            <th scope="col">WEBSITE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {api.map((item) => (
+            <tr>
+              <th scope="row">{item.id}</th>
+              <td>{item.name}</td>
+              <td>{item.username}</td>
+              <td>{item.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default Api
+export default Api;
